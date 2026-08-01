6b3d72ea65215d9c8600b209149e1ee7b68ef275
sulibethrodriguezreyes-ship-it <sulibethrodriguezreyes@gmail.com>
2026-07-31 21:20:27 -0400

    this is my third feature list tasks

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,23 @@
-// Project setup - no functionality yet
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const text = input.value.trim();
+
+    // Validación
+    if (text === "") return;
+
+    // Crear elemento li
+    const li = document.createElement("li");
+
+    // Crear span para el texto
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    // Agregar el span al li
+    li.appendChild(span);
+
+    // Agregar el li a la lista
+    document.getElementById("taskList").appendChild(li);
+
+    // Limpiar input
+    input.value = "";
+}
